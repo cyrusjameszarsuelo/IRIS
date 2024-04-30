8ef20047c3b0550806c7a8b43fb57d858f378b36
katebolanos <32135090+katebolanos@users.noreply.github.com>
2024-04-30 22:00:44 +0800

    Client Interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { AsideComponent } from './includes/aside/aside.component';
 import { NavbarComponent } from './includes/navbar/navbar.component';
 import { FooterComponent } from './includes/footer/footer.component';
+import { TitlePageComponent } from './includes/title-page/title-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsideComponent, NavbarComponent, FooterComponent, RouterLink],
+  imports: [RouterOutlet, AsideComponent, NavbarComponent, FooterComponent, RouterLink, TitlePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
